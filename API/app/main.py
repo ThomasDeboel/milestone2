7899f0c00accfdcc9a548d420192bf9180e49105
Thomas Deboel <r0841840@student.thomasmore.be>
2023-12-10 21:17:10 +0100

    Update Dockerfiles and API code

--- API/app/main.py
+++ API/app/main.py
@@ -2,11 +2,10 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from pymongo import MongoClient
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import HTTPException
 
 app = FastAPI()
 
-
+# Enable CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -18,23 +17,26 @@ app.add_middleware(
 class User(BaseModel):
     name: str
 
-
-client = MongoClient("mongodb://root:example@mongo-td-service:27017/")
+# Connect to MongoDB
+client = MongoClient("mongodb://root:example@db:27017/")
 db = client["mydatabase"]
 collection = db["users"]
 
 @app.get("/user")
 async def get_user():
+    # Retrieve a user from the database
     user = collection.find_one()
     return {"username": user["name"]}
 
 @app.post("/user")
 async def create_user(user: User):
+    # Create a new user in the database
     user_data = {"name": "Thomas Deboel"}
     collection.insert_one(user_data)
     return user
 
 @app.post("/changeuser")
 async def update_user():
+    # Update the name of a user in the database
     collection.update_one({"name": "Thomas Deboel"}, {"$set": {"name": "Frietje Jansens"}})
     return {"message": "User updated"}
